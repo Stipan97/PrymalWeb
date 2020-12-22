93a31581836328c7ebb763e026d3a7b70ae652fe
Stjepan Posavec <stjepan.posavec@gmail.com>
2020-12-22 13:30:34 +0100

    upload to storage - test

--- src/components/TEST.tsx
+++ src/components/TEST.tsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { FC } from 'react';
+import { storage } from '../configs/firebase';
 
 export const Test: FC = () => {
-  return <></>;
+  let file: File | null | undefined;
+
+  const onChangeFile = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.currentTarget.files?.item(0));
+
+    file = event.currentTarget.files?.item(0);
+  };
+
+  const onClickUpload = () => {
+    if (file) {
+      storage
+        .ref('test/' + file.name)
+        .put(file)
+        .then((snapshot) => {
+          snapshot.ref.getDownloadURL().then((url) => {
+            console.log(url);
+          });
+        });
+    }
+  };
+
+  return (
+    <>
+      <input onChange={onChangeFile} type="file" id="myfile" name="myfile" />
+      <button onClick={onClickUpload}>Upload</button>
+    </>
+  );
 };
